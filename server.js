067d51d8987075c61d299df3f318a1fdb709e042
mansour <davudimansur@gmail.com>
2019-03-26 01:55:03 -0700

    port changed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const hbs=require('hbs');
 const path=require('path');
 const app=express();
+const port=3000;
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname,'public')));
 hbs.registerPartials(path.join(__dirname,'views/partials'));
@@ -10,10 +11,10 @@ hbs.registerHelper('date',()=>{
 });
 app.get('/',(req,res)=>{
     //res.send('hello');
-    res.render('home',{
+    res.render('home',{ 
         title:'صفحه اصلی'
     });
 });
-app.listen(3000,()=>{
-    console.log('server is running');
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
 });
